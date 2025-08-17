c1cc11a14579594062c01701a7bf90a7649647a2
bambamboole <manuel.christlieb@xentral.com>
2025-08-17 17:50:50 +0200

    publish ai guidelines via vendor:publish

--- src/TestingServiceProvider.php
+++ src/TestingServiceProvider.php
@@ -9,6 +9,9 @@ class TestingServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->mergeConfigFrom(__DIR__.'/../config/testing.php', 'testing');
+        $this->publishes([
+            dirname(__DIR__).'/.ai/guidelines/xentral-testing.blade.php' => base_path('.ai/guidelines/xentral-testing.blade.php'),
+        ], 'xentral-testing');
     }
 
     public function register(): void {}
